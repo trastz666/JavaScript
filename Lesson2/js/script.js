a8c19d5d6fc66f875688becae38e8e5c3f35f068
Evgeniy <trastz666@gmail.com>
2018-06-03 15:01:33 +0300

    Третье задание

--- Lesson2/js/script.js
+++ Lesson2/js/script.js
@@ -1,7 +1,20 @@
-let youBudget = prompt("Ваш бюджет?"),
-	nameScore = prompt("Название вашего магазина?"),
-	time = 8;
+let youBudget, 
+	nameScore ,
+	time ,
+	price
 	
+function start() {
+	 youBudget = prompt("Ваш бюджет?");
+
+	 while (isNaN(youBudget) || youBudget == "" || youBudget == null) {
+	 	youBudget = prompt("Ваш бюджет?");
+	 }
+
+	 nameScore = prompt("Название вашего магазина?").toUpperCase();
+
+	 time = 8;
+}
+start()
 
 console.log(youBudget); 
 console.log(nameScore);
@@ -12,10 +25,12 @@ let mainList = {
 	nameShop  : nameScore,
 	shopGoods :[],
 	employers : {},
-	open: false
+	open: false,
+	discount: true
 
 };
 
+function chooseGoods() {
 	for(let i = 0; i < 5; i++){
 
 	let a = prompt("Какой тип товара будем продавать?");
@@ -28,53 +43,46 @@ let mainList = {
 		--i;
 	}
 } 
+}
+chooseGoods();
 
-
-
-
-//Первый способ
-/*let i = 0;
-while(i < 5) {
-	console.log(i);
-	i++;
-	let a = prompt("Какой тип товаров будем продавать?");
- 		mainList.shopGoods[i] = a;
- 		if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50) {
- 		console.log('Все верно');
- 	} else {
- 		--i;
- 	}
- 	
-}; 
-*/
-
-
-
-
-
-//Второй способ
-
-/*let i = 0;
- 	do {
-		console.log(i);
-		i++;
-		let a = prompt("Какой тип товаров будем продавать?");
-		mainList.shopGoods[i] = a;
-		if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50) {
- 		console.log('Все верно');
- 	} else {
- 		--i;
- 	}
- 	} while(i < 5);*/
-
-
+function workTime(time) {
+if (time < 0) {
+	console.log('Этого не может быть!');
+} else if (time  >=  8 && time <= 20) {
+	console.log('Пора работать');
+	}	else if (time < 24) {
+			console.log('Магазин закрыт');
+		}		else if (time > 24) {
+			console.log("В сутках 24 часа");
+			}
+}
+workTime(1);
 
 console.log(mainList);
 
-let budgetDaysMonth  = 30,
-	budgetDay        = youBudget/budgetDaysMonth;
+let budgetDaysMonth = 30;
+budgetDay = function(youBudget,budgetDaysMonth) {
+	return youBudget/budgetDaysMonth;
+}
+alert("Ваш дневной бюджет из расчета 30 дней:" + budgetDay(youBudget, budgetDaysMonth));
 
-	alert("Ваш бюджет на один день:" + budgetDay);
+function discontSystem(price) {
+	if (mainList.discount === true) {
+		alert("Цена со скидкой" + price*80/100); 
+	}else if (mainList.discount === false) {
+		alert("Цена без скидки" + price);
+	}
+}
+discontSystem(5000);
+
+function employers() {
+	for(let i = 0; i < 4; i++ )	{
+		let name = prompt("Введите имя сотрудника?");
+		mainList.employers[i] = name;
+	}
+}
+employers();
 
 
 
