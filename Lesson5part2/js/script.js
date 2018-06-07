945c80946e0190ffd97dd67a9b88deb7ed87515a
Evgeniy <trastz666@gmail.com>
2018-06-07 14:23:26 +0300

    Это 5 урок

---
+++ Lesson5part2/js/script.js
@@ -0,0 +1,22 @@
+let openShop = document.getElementById('open-btn'),
+	nameShop = document.getElementsByClassName('name'),
+	budgetShop = document.getElementsByClassName('budget'),
+	goodsShop = document.getElementsByClassName('goods'),
+	itemsShop = document.getElementsByClassName('items'),
+	employersShop = document.getElementsByClassName('employers'),
+	discountShop = document.getElementsByClassName('discount'),
+	isopenShop =document.getElementsByClassName('isopen'),
+	// поля категории товаров
+	inputGoods = document.getElementsByClassName('goods-item'),
+	inputProduct = document.querySelector('.choose-item'),
+	inputTime = document.querySelector('.time-value'),
+	inputBudgetDay = document.querySelector('.count-budget-value')
+	inputEmplours = document.querySelectorAll('hire-employers-item'),
+	//Получаем кнопки 
+	buttonOpenShop = document.getElementById('open-btn'),
+	approveButton = document.getElementsByTagName[2],
+	calculateButton = document.getElementsByTagName[3],
+	hireButton = document.getElementsByTagName[4];
+
+
+	console.log(nameShop);
